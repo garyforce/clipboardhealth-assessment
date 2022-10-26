06c0222d32f19c47d4cc6668afbb73d8a30e13e1
Gary Foster <gary.work.pro@gmail.com>
2022-10-25 22:28:57 -0400

    complete coding

--- dpk.js
+++ dpk.js
@@ -1,28 +1,23 @@
 const crypto = require("crypto");
 
+const createHashfromKey = (key) => {
+  return crypto.createHash("sha3-512").update(key).digest("hex");
+};
+
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let partitionKey = "0";
 
-  if (event) {
+  if (event && typeof event === "object") {
     if (event.partitionKey) {
-      candidate = event.partitionKey;
+      partitionKey = event.partitionKey;
+      partitionKey =
+        partitionKey.length > MAX_PARTITION_KEY_LENGTH
+          ? createHashfromKey(partitionKey)
+          : partitionKey;
     } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+      partitionKey = createHashfromKey(JSON.stringify(event));
     }
   }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  return partitionKey;
 };
--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,21 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("Returns the hash value when the input length is bigger than 256", () => {
+    const event = {
+      partitionKey:
+        "a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0a1a2a3a4a5a6a7a8a9a0",
+    };
+    const trivialKey = deterministicPartitionKey(event);
+
+    expect(trivialKey.length).toBe(128);
+  });
+  it("Returns the same partition key when the input length is smaller than 256", () => {
+    const event = {
+      partitionKey: "partitionKey",
+    };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBe("partitionKey");
+  });
 });
